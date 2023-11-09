3e9cb172ee1c376e28762195cba5f5e792c84dd4
Vasileios Lagopoulos <Vasileios.Lagopoulos@pfizer.com>
2023-11-10 00:38:41 +0200

    Commentary on main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Elevator import Elevator
 from Generator import Generator
 
-
+# Checks if all people have been served - true/false
 def finished():
   passengers = 0
   for i in header:
@@ -12,6 +12,7 @@ def finished():
   else:
     return True
 
+# Returns total(int) number of passengers that are waiting in the queue 
 def count_passengers():
   passengers = 0
   for i in header:
@@ -19,11 +20,12 @@ def count_passengers():
       passengers = passengers + 1
   return passengers
 
+# Print status of people waiting in each floor for visualization purposes
 def print_status():
   for i in header:
     print(i)
 
-
+# Initialization of values - elevator starts from 0
 floor = 0
 num_of_floors = -1
 elevator_capacity = -1
@@ -65,13 +67,20 @@ with open(f"./queues{num_of_floors}.txt", "r") as file:
   count = 0
   k = file.readlines()
   for i in k:
+    # Reading the line with split in order to get values seperated by comma .split(",")
+    # and "throwing" the change of line char .strip("\n")
     header[count] = [*i.strip("\n").split(',')]
     count = count + 1
 
+# Initializing the elevator object using Elevator class 
 elevator = Elevator(elevator_capacity, num_of_floors)
 
 
-
+# General idea of elevator is that while there are passengers to serve
+# 1. The elevator gets people who are willing to move to its direction as long as they fit in elevator's capacity
+# 2. The elevator moves, floor by floor, in the direction it should (see Generator.py for the checks made and the logic)
+# 3. The elevator leaves people to their wanted floor and then the iteration begins again 
+# 4. Print report is used for visualization purposes after getting and leaving people
 while(not finished()):
     for p in header[floor].copy():
         if(elevator.get_people(int(p))):
